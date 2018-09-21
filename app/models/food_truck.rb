0d7f62783159554195649fe31360f76c69641a51
Francois Belle <belle.francois.38@gmail.com>
2018-09-21 14:16:07 -0400

    FoodTruck model added (non active record)

--- app/models/food_truck.rb
+++ app/models/food_truck.rb
@@ -1,2 +1,25 @@
-class FoodTruck < ApplicationRecord
+# frozen_string_literal: true
+
+class FoodTruck
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+
+  attr_accessor :latitude,
+                :longitude,
+                :address,
+                :name,
+                :food_items,
+                :location_description,
+                :schedule_short,
+                :schedule_url
+
+  validates :latitude, :longitude, :address, :name, presence: true
+  validates :latitude, :longitude, numericality: true
+
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
 end
